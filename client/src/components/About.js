e87845e218be3bf6b18d20450249479e5fe64775
root <root@US-SGHOSH.fortinet-us.com>
2020-11-27 02:48:24 -0800

    set up route

---
+++ client/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const About =()=>{
+    return(
+        <div>
+            <h3> About </h3>
+            <p> This is the About section</p>
+        </div>
+    )
+}
+
+export default About
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+
+const Navbar = ()=>{
+    return(
+        <nav>
+            <div>
+                <ul>
+                    <li><Link to='/home'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/mongoForm'>MongoForm/AdminLogin</Link></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
